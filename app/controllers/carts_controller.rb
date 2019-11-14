b46af4bef8d8dd8d7d98a411cf519535f7ec8221
Juan Carrera <juankarrera@gmail.com>
2019-11-14 14:32:33 -0500

    fix the empty cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -19,7 +19,7 @@ class CartsController < ApplicationController
 
   private
 
-  def modify_cart_delta(product_id, delta)
+  def modify_cart_delta product_id, delta
     cart[product_id] = (cart[product_id] || 0) + delta
     cart.delete(product_id) if cart[product_id] < 1
     update_cart cart
